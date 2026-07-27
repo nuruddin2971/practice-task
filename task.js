31baf03f447f3f05fbb97c6496d6daa2e299d0fd
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-28 05:05:28 +0600

    first commit

---
+++ task.js
@@ -0,0 +1,42 @@
+// Task-1
+
+// Input: a number
+// Output: "Hot", "Cold", or "Normal"
+// Returns: a string
+
+// function checkTemperature(temp) {
+//   // TODO: write your logic here
+//   if (temp >= 30) {
+//     return "Hot";
+//   } else if (temp <= 15) {
+//     return "Cold";
+//   } else {
+//     return "Normal";
+//   }
+// }
+
+// console.log(checkTemperature(35)); // Expected: "Hot"
+// console.log(checkTemperature(10)); // Expected: "Cold"
+// console.log(checkTemperature(20)); // Expected: "Normal"
+
+// Task-2
+
+// Input: a number
+// Output: the number with digits reversed
+// Returns: a number
+
+// function reverseNumber(num) {
+//   let str = num.toString();
+//   let reversedNum = "";
+//   // TODO: build the reversed string, then convert back to a number
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedNum += str[i];
+//   }
+//   return parseInt(reversedNum);
+// }
+
+// console.log(reverseNumber(1234)); // Expected: 4321
+// console.log(reverseNumber(7)); // Expected: 7
+
+// Task-3
+
